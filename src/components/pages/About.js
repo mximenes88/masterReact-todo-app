fe9f198b8ea4f18a31720ca121cd669f062a955f
Morgana Ximenes <morgana.ximenes@yahoo.ca>
2019-04-19 19:26:02 -0400

    Finalized app

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function about() {
+  return (
+    <React.Fragment>
+        <h1>About</h1>
+        <p>This is the TodoList app v1.0.0. It is part of my React Exploration Journey</p>
+      
+    </React.Fragment>
+  )
+}
+
+export default about;
