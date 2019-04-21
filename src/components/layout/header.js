abebd60d2b10f56605d3c2e0d15e2e987e89593c
Morgana Ximenes <morgana.ximenes@yahoo.ca>
2019-04-21 10:21:22 -0400

    minor styling : header color & about me updated

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Header(){
     return (
         <header style={headerStyle}>
-            <h1>TodoList</h1>
+            <h1>TaskMaster</h1>
             <Link  style={linkStyle} to="/">Home</Link> | <Link  style={linkStyle} to="/about">About</Link>
         </header>
     )
@@ -13,12 +13,12 @@ function Header(){
 
  const linkStyle={
    color: '#fff',
-   textDecoation:'none'
+   textDecoration:'none'
  }
 
 
  const headerStyle ={
-     background: '#333',
+     background: 'darkblue',
      color: '#fff',
      textAlign:'center',
      padding:'10px'
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,9 @@ function about() {
   return (
     <React.Fragment>
         <h1>About</h1>
-        <p>This is the TodoList app v1.0.0. It is part of my React Exploration Journey</p>
-      
+        <p>This is the TaskMaster app v1.0.0. It is part of my React Exploration Journey.</p>
+        <p>Feel free to explore my other projects at <a href="https://github.com/mximenes88">Github Repos</a></p>
+  
     </React.Fragment>
   )
 }
